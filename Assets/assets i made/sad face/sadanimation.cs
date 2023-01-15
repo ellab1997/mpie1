2afdf5a8e979ff0b6b38c81e153f448d1e6a5185
ellab1997 <eb1997@york.ac.uk>
2023-01-15 07:42:39 +0000

    4

---
+++ Assets/assets i made/sad face/sadanimation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sadanimation : MonoBehaviour
+{
+    bool isActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isActive = true;
+        Animator anim = GetComponent<Animator>();
+        anim.SetBool("go", isActive);
+    }
+}
