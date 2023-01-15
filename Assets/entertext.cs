1e4d60318d08c1a96b6ae9a00aa6626f376891fe
ellab1997 <eb1997@york.ac.uk>
2023-01-15 17:09:05 +0000

    2

--- Assets/entertext.cs
+++ Assets/entertext.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-//code from https://www.youtube.com/watch?v=guelZvubWFY&ab_channel=GameDevTraum
+//text input code from https://www.youtube.com/watch?v=guelZvubWFY&ab_channel=GameDevTraum
 
 
 public class entertext : MonoBehaviour
@@ -10,10 +10,12 @@ public class entertext : MonoBehaviour
     bool activateGoodAnimation;
     bool activateBadAnimation;
     private string input;
+    public GameObject inputBox;
     // Start is called before the first frame update
     void Start()
     {
         activateGoodAnimation = false;
+        activateBadAnimation = false;
     }
 
     // Update is called once per frame
@@ -29,15 +31,22 @@ public class entertext : MonoBehaviour
             anim.SetBool("", activateBadAnimation);
         }
     }
-
+//this from tutorial
     public void textInput(string newText){
         input = newText;
+//to this
         Debug.Log(input);
         input.ToLower();
         if(input == "i love you" || input == "be strong" || input == "you're amazing" || input == "youre amazing" || input == "grow well" || input == "stay healthy"){
             activateGoodAnimation = true;
-        } else {
+            Debug.Log(activateGoodAnimation);
+            Destroy(inputBox);
+        } else if (input == "i hate you" || input == "be weak" || input == "you're awful" || input == "youre awful" || input == "grow badly" || input == "be unhealthy") {
             activateBadAnimation = true; 
+            Debug.Log(activateBadAnimation);
+            Destroy(inputBox);
+        } else {
+
         }
     }
 }
