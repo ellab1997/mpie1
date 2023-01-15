eadbea0ed6aa8e68a9ada7242ec35582a08cb01c
ellab1997 <eb1997@york.ac.uk>
2023-01-15 15:49:40 +0000

    2

--- Assets/talking.cs
+++ Assets/talking.cs
@@ -10,28 +10,35 @@ public class talking : MonoBehaviour
     public GameObject textobject2;
     public GameObject textobject3;
     public GameObject trigger;
+    bool isActive;
     // Start is called before the first frame update
     void Start()
     {
         textobject1.SetActive(false);
         textobject2.SetActive(false);
         textobject3.SetActive(false);
+        isActive = false
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isActive == true){
+            Animator anim = GetComponent<Animator>();
+            anim.SetBool("talking", isActive);
+        }
     }
     void OnTriggerEnter(Collider other){
         textobject1.SetActive(true);
         textobject2.SetActive(true);
         textobject3.SetActive(true);
+        isActive = true;
     }
 
     void OnTriggerExit(Collider other){
         textobject1.SetActive(false);
         textobject2.SetActive(false);
         textobject3.SetActive(false);
+        isActive = false;
     }
 }
