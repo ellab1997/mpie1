c2a33608f54e25d95b7f515cb914674585914a8f
ellab1997 <eb1997@york.ac.uk>
2023-01-16 02:03:29 +0000

    5

--- Assets/blueinput2.cs
+++ Assets/blueinput2.cs
@@ -7,33 +7,33 @@ using UnityEngine.UI;
 
 public class blueinput2 : MonoBehaviour
 {
-    bool activateGoodAnimation;
+    public bool goodAnimation = false;
     bool activateBadAnimation;
     private string input;
     public GameObject inputBox;
     public GameObject mainObject;
-    public GameObject previous;
+    public GameObject toDestroy;
+    AudioSource audio;
 
     // Start is called before the first frame update
     void Start()
     {
-        activateGoodAnimation = false;
+        goodAnimation = false;
         activateBadAnimation = false;
-        mainObject.SetActive(false);
+        mainObject.SetActive(true);
         inputBox.SetActive(true);
-        previous.SetActive(true);
+        audio = GetComponent<AudioSource>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(activateGoodAnimation == true){
-            previous.SetActive(false);
+        if(goodAnimation == true){
             mainObject.SetActive(true);
             Animator anim = GetComponent<Animator>();
-            anim.SetBool("first", activateGoodAnimation);
-            anim.Play("Take 001");
+            anim.SetBool("second", goodAnimation);
+            Destroy(toDestroy);
         } 
 
         if(activateBadAnimation == true){
@@ -50,7 +50,8 @@ public class blueinput2 : MonoBehaviour
         Debug.Log(input);
         input.ToLower();
         if(input == "i love you" || input == "be strong" || input == "you're amazing" || input == "youre amazing" || input == "grow well" || input == "stay healthy"){
-            activateGoodAnimation = true;
+            goodAnimation = true;
+            audio.Play();
             Destroy(inputBox);
         } else if (input == "i hate you" || input == "be weak" || input == "you're awful" || input == "youre awful" || input == "grow badly" || input == "be unhealthy") {
             activateBadAnimation = true; 
